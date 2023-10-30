f326cd743cf502aecc19d0273954ea07a895f8a8
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-10-30 11:34:10 -0500

    Restart project from scratch

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-// Express.js web server
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.db");
 
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Create a table
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, name TEXT)");
 });
+
+// Insert data
+db.serialize(() => {
+  const stmt = db.prepare("INSERT INTO my_table (name) VALUES (?)");
+  stmt.run("John");
+  stmt.finalize();
+});
+
+// Query data
+db.all("SELECT * FROM my_table", (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
+
+// Close the database connection
+db.close();
--- models/db.js
+++
@@ -1,8 +0,0 @@
-// SQLite database
-
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('emails.db');
-
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS emails (email TEXT)");
-});
