3dd9d0dad7ee4bed4cc244a1338eabe732b4be45
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-10-30 13:25:17 -0500

    Update comments to be more explanatory

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("database.db");
+const sqlite3 = require("sqlite3").verbose(); // Import SQLite in Node.js enviroment
+const db = new sqlite3.Database("database.db"); // Create new database
 
-// Create a table
+// Create table named my_table with columns 'id', 'name', and 'email'
 db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, name TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, name TEXT, email TEXT)");
 });
 
 // Insert data
 db.serialize(() => {
-  const stmt = db.prepare("INSERT INTO my_table (name) VALUES (?)");
-  stmt.run("John");
+  const stmt = db.prepare("INSERT INTO my_table (name, email) VALUES (?, ?)"); // Data entry 'recipe'
+  stmt.run("John", "john@example.com"); // Specific data entry
   stmt.finalize();
 });
 
@@ -18,6 +18,8 @@ db.all("SELECT * FROM my_table", (err, rows) => {
   if (err) {
     throw err;
   }
+
+  // rows is a list of objects representing each data row
   rows.forEach((row) => {
     console.log(row);
   });
